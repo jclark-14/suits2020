08d2251614a8af34c0266a11b23c5a0dbe0409f3
Jody Clark <jodyclark09@gmail.com>
2024-11-01 12:57:37 -0400

    Minor adjustment to carousel bg color.

--- components/layout/HeroCarousel.tsx
+++ components/layout/HeroCarousel.tsx
@@ -191,7 +191,7 @@ function HeroCarousel() {
   }, [isTransitioning, handleTransitionEnd]);
 
   return (
-    <div className="relative h-[275px] sm:h-[350px] md:h-[415px] lg:h-[500px] w-full overflow-hidden bg-black">
+    <div className="relative h-[275px] sm:h-[350px] md:h-[415px] lg:h-[500px] w-full overflow-hidden bg-gray-800">
       <div
         className={`flex h-full ${
           isTransitioning
