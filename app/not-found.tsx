bcd72ebdf13f772fad7d4c01752cf42978068174
Jody Clark <jodyclark09@gmail.com>
2024-11-03 19:49:48 -0500

    Adjusted background color for 404 page for mobile.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,18 +3,15 @@ import Link from 'next/link';
 
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center min-h-[500px]">
+    <div className="flex items-center justify-center min-h-[500px] bg-white">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
         <p className="text-xl text-gray-800 my-2">
           Oops! The page you&apos;re looking for doesn&apos;t exist...yet
         </p>
-        <p className="text-gray-800 text-lg">
-          Back to{' '}
-          <Link href="/" className="font-extrabold text-xl hover:underline">
-            Home
-          </Link>
-        </p>
+        <Link href="/" className="font-bold text-xl hover:underline">
+          Back to Home
+        </Link>
       </div>
     </div>
   );
