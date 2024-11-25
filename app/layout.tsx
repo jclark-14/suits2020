7478aa36f8aca18b262c040a1754aa1df68cfb5a
Jody Clark <jodyclark09@gmail.com>
2024-11-25 15:29:28 -0500

    Added Google Analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import { Metadata } from 'next';
 import { defaultMetadata } from '@/metadata.config';
-import Head from 'next/head';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -20,27 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.className}`}>
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9RKB9096ED"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-9RKB9096ED', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+      <head>
         <link rel="canonical" href={metadata.metadataBase?.toString()} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      </head>
+      <GoogleAnalytics />
       <body className="flex flex-col min-h-screen bg-black lg:bg-white">
         <Header />
         <main role="main" className="flex-grow ">
---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,27 @@
+// components/GoogleAnalytics.tsx
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9RKB9096ED');
+          `,
+        }}
+      />
+    </>
+  );
+}
